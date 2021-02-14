7fb9d84035270868e68cc5e1e980943a026a4056
4UR3L10 <aureliomartinezdros@outlook.com>
2021-02-14 15:19:30 -0500

    Init

--- backend/server/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-
-app.listen(process.env.PORT || "3000", () => {
-  console.log("Server is running on port: ${process.env.PORT || '3000'}");
-});
