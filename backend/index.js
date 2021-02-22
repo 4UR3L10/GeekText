571cbc888d2cbf1dea024b3c1987bd19ea272d6a
4UR3L10 <aureliomartinezdros@outlook.com>
2021-02-21 23:23:16 -0500

    Server Running

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const signUpRoute = require("./routes/signup");
 const app = express();
-const cors = require("cors");
 
 // Json form send data , you need to use URL enoded request bodies.
 app.use(express.json());
@@ -13,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 // Call SignUp Section.
 app.use("/signup", signUpRoute);
 
-// Run the Server.
+// Run the Server PORT 3001.
 app.listen(3001, () => {
-  console.log("Server is running on port: 3001}");
+  console.log("Server is running on port: 3001");
 });
---
+++ backend/routes/signup.js
@@ -0,0 +1,5 @@
+const { Router } = require("express");
+const db = require("../db");
+const router = Router();
+
+module.exports = router;
