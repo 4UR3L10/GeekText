924c7d5ace57c59f3c3daf5c44576a61ac61151c
getrod <kevinlouisjean@gmail.com>
2021-03-05 14:23:51 -0500

    added error handling to book/:id route

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -9,19 +9,36 @@ router.get('/', function (req, res) {
   const queryString = `SELECT * FROM geektext.book`;
   mysqlx.getSession(credentials)
     .then(session => session.sql(queryString).execute())
-    .then(result => res.json(queryResultToJson(result)))
+    .then(result => queryResultToJson(result))
+    .then(result => res.json(result))
     .catch((err) => console.log(err));
 });
 
 
 router.get('/:id', (req, res) => {
-  const queryString = `SELECT * FROM geektext.book WHERE id = '${req.params.id}'`;
+  // Validate input
+  const book_id = req.params.id
+  if (!parseInt(book_id)) {
+    console.log('ho')
+    return res.status(400).send(`Invalid input: ${book_id}`)
+  }
+
+  const queryString = `SELECT * FROM geektext.book WHERE id = '${book_id}'`;
   mysqlx.getSession(credentials)
     .then(session => session.sql(queryString).execute())
+    .then(result => queryResultToJson(result))
     .then(result => {
-      res.json(queryResultToJson(result));
+      // If not found, send 404 error
+      if (result.length == 0) {
+        return res.status(404).send(`Book with the given id is not found`)
+      }
+      return res.json(result)
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err)
+      // If you don't know what happened, send 500
+      res.status(500).send('Server Error')
+    });
 });
 
 module.exports = router
