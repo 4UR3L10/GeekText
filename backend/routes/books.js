ccfb7fec28f08fbd1e410cb9101937ed38e87f8c
getrod <kevinlouisjean@gmail.com>
2021-03-15 12:07:18 -0400

    added more data retured from books affected routes are /books and books/{id}

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -4,9 +4,27 @@ const credentials = require('./credentials');
 const { queryResultToJson } = require('./util');
 const router = express.Router();
 
+/**
+ * book schema: {
+ *  book_id,
+ *  title,
+ *  description,
+ *  book_cover,
+ *  author_name,
+ *  author_bio,
+ *  genre,
+ *  avg_rating,
+ *  publisher_name,
+ *  publisher_date,
+ * }
+ */
 
 router.get('/', function (req, res) {
-  const queryString = `SELECT * FROM geektext.book`;
+  const queryString = `
+  SELECT w.book_id, book_title, description, price, b.cover, genre, avg_rating, author_name, author_bio, publisher_name, published_date
+  FROM geektext.author_wrote_book w, geektext.book b, geektext.author a, geektext.book_published bp, geektext.publisher p
+  WHERE w.author_id = a.id AND w.book_id = b.id AND bp.book_id = b.id AND bp.publisher_id = p.id;
+  `;
   mysqlx.getSession(credentials)
     .then(session => session.sql(queryString).execute())
     .then(result => queryResultToJson(result))
@@ -22,7 +40,11 @@ router.get('/:id', (req, res) => {
     return res.status(400).send(`Invalid input: ${book_id}`)
   }
 
-  const queryString = `SELECT * FROM geektext.book WHERE id = '${book_id}'`;
+  const queryString = `
+  SELECT w.book_id, book_title, description, price, b.cover, genre, avg_rating, author_name, author_bio, publisher_name, published_date
+  FROM geektext.author_wrote_book w, geektext.book b, geektext.author a, geektext.book_published bp, geektext.publisher p
+  WHERE b.id = '${book_id}' AND w.author_id = a.id AND w.book_id = b.id AND bp.book_id = b.id AND bp.publisher_id = p.id;
+  `;
   mysqlx.getSession(credentials)
     .then(session => session.sql(queryString).execute())
     .then(result => queryResultToJson(result))
