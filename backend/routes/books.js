41aaee802de449bb5ea7756430f0833fefc42310
getrod <kevinlouisjean@gmail.com>
2021-03-15 15:14:29 -0400

    added books/{id}/reviews route

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -5,7 +5,7 @@ const { queryResultToJson } = require('./util');
 const router = express.Router();
 
 /**
- * book schema: {
+ * books/id: {
  *  book_id,
  *  title,
  *  description,
@@ -93,6 +93,39 @@ router.get('/:id/authors', (req, res) => {
       res.status(500).send('Server Error')
     });
 });
+
+router.get('/:id/reviews', (req, res) => {
+  // Validate input
+  const book_id = req.params.id
+  if (!parseInt(book_id)) {
+    return res.status(400).send(`Invalid input: ${book_id}`)
+  }
+
+  const queryString = `
+  SELECT *
+  FROM geektext.user_book_review
+  WHERE book_id = '${book_id}';
+  `;
+  
+  mysqlx.getSession(credentials)
+    .then(session => session.sql(queryString).execute())
+    .then(result => queryResultToJson(result))
+    .then(result => {
+      // If not found, send 404 error
+      if (result.length == 0) {
+        return res.status(404).send(`Book with the given id is not found`)
+      }
+      return res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      // If you don't know what happened, send 500
+      res.status(500).send('Server Error')
+    });
+});
+
+
+
 
 module.exports = router
 //https://expressjs.com/en/guide/routing.html
