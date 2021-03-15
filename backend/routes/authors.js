bc791d729be275558f945ba8fcb6a5b904656f41
getrod <kevinlouisjean@gmail.com>
2021-03-15 15:28:49 -0400

    added authors/{id}/books route

--- backend/routes/authors.js
+++ backend/routes/authors.js
@@ -21,4 +21,28 @@ router.get('/:id', function (req, res) {
     .catch((err) => console.log(err));
 });
 
+router.get('/:id/books', function (req, res) {
+  const author_id = req.params.id
+  if (!parseInt(author_id)) return res.status(400).send(`Invalid input: ${author_id}`)
+  
+
+  const queryString = `
+  SELECT w.book_id, book_title, description, price, b.cover, genre, avg_rating, author_name, author_bio, publisher_name, published_date
+  FROM geektext.author_wrote_book w, geektext.book b, geektext.author a, geektext.book_published bp, geektext.publisher p
+  WHERE a.id = ${author_id} AND w.author_id = a.id AND w.book_id = b.id AND bp.book_id = b.id AND bp.publisher_id = p.id;
+  `;
+  
+  mysqlx.getSession(credentials)
+    .then(session => session.sql(queryString).execute())
+    .then(result => queryResultToJson(result))
+    .then(result => {
+      if (result.length == 0) return res.status(404).send(`Book with the given id is not found`)
+      return res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).send('Server Error')
+    });
+});
+
 module.exports = router
