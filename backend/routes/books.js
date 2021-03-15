124c79144e0ea45b8fb21ff8c01121454fb24b62
getrod <kevinlouisjean@gmail.com>
2021-03-15 12:39:06 -0400

    added books/{id}/authors route

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -45,6 +45,38 @@ router.get('/:id', (req, res) => {
   FROM geektext.author_wrote_book w, geektext.book b, geektext.author a, geektext.book_published bp, geektext.publisher p
   WHERE b.id = '${book_id}' AND w.author_id = a.id AND w.book_id = b.id AND bp.book_id = b.id AND bp.publisher_id = p.id;
   `;
+  
+  mysqlx.getSession(credentials)
+    .then(session => session.sql(queryString).execute())
+    .then(result => queryResultToJson(result))
+    .then(result => {
+      // If not found, send 404 error
+      if (result.length == 0) {
+        return res.status(404).send(`Book with the given id is not found`)
+      }
+      return res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      // If you don't know what happened, send 500
+      res.status(500).send('Server Error')
+    });
+});
+
+
+router.get('/:id/authors', (req, res) => {
+  // Validate input
+  const book_id = req.params.id
+  if (!parseInt(book_id)) {
+    return res.status(400).send(`Invalid input: ${book_id}`)
+  }
+
+  const queryString = `
+  SELECT w.book_id, w.author_id, author_name, author_bio
+  FROM geektext.author_wrote_book w, geektext.book b, geektext.author a
+  WHERE b.id = '${book_id}' AND w.author_id = a.id AND w.book_id = b.id;
+  `;
+  
   mysqlx.getSession(credentials)
     .then(session => session.sql(queryString).execute())
     .then(result => queryResultToJson(result))
