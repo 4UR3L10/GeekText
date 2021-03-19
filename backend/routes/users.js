25eca39afd9b3757fc9c132bdff108798506b5ba
getrod <kevinlouisjean@gmail.com>
2021-03-19 00:23:06 -0400

    added users/:id/cart route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -40,4 +40,32 @@ router.get('/:id', function (req, res) {
         });
 });
 
+router.get('/:id/cart', function (req, res) {
+    const user_id = req.params.id
+    if (!parseInt(user_id)) {
+        return res.status(400).send(`Invalid user id input: ${user_id}`)
+    }
+
+    const queryString = `
+    SELECT b.book_title, b.cover, a.author_name, b.price, sh.cart_quantity
+    FROM geektext.shopping_cart sh, geektext.book b, geektext.author_wrote_book w, geektext.author a
+    WHERE sh.user_id = ${user_id} AND sh.book_id = b.id AND w.book_id = b.id AND w.author_id = a.id
+    `;
+
+    mysqlx.getSession(credentials)
+        .then(session => session.sql(queryString).execute())
+        .then(result => queryResultToJson(result))
+        .then(result => {
+            if (result.length == 0) return res.status(404).send(`User with id ${user_id} is not found`)
+            return res.json(result)
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).send('Server Error')
+        });
+});
+
+
+
+
 module.exports = router
