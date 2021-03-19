34e4f9ccdd1e3ad817888b237743ee8fbb1c5962
getrod <kevinlouisjean@gmail.com>
2021-03-19 00:00:07 -0400

    added users/ get http method

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,15 @@ const { queryResultToJson } = require('./util');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.send('This is the users page!!');
+    const queryString = `SELECT * FROM geektext.user`;
+    mysqlx.getSession(credentials)
+      .then(session => session.sql(queryString).execute())
+      .then(result => queryResultToJson(result))
+      .then(result => res.json(result))
+      .catch((err) => {
+        console.log(err)
+        res.status(500).send('Server Error')
+      });
 });
 
 module.exports = router
