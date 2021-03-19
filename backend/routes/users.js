21fc814dfa5bf88e688ef1cad1f96c7931702a92
getrod <kevinlouisjean@gmail.com>
2021-03-19 00:08:12 -0400

    added users/:id route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,13 +7,37 @@ const router = express.Router();
 router.get('/', function (req, res) {
     const queryString = `SELECT * FROM geektext.user`;
     mysqlx.getSession(credentials)
-      .then(session => session.sql(queryString).execute())
-      .then(result => queryResultToJson(result))
-      .then(result => res.json(result))
-      .catch((err) => {
-        console.log(err)
-        res.status(500).send('Server Error')
-      });
+        .then(session => session.sql(queryString).execute())
+        .then(result => queryResultToJson(result))
+        .then(result => res.json(result))
+        .catch((err) => {
+            console.log(err)
+            res.status(500).send('Server Error')
+        });
+});
+
+router.get('/:id', function (req, res) {
+    const user_id = req.params.id
+    if (!parseInt(user_id)) {
+        return res.status(400).send(`Invalid user id input: ${user_id}`)
+    }
+
+    const queryString = `
+    SELECT * FROM geektext.user 
+    WHERE id = ${user_id}
+    `;
+
+    mysqlx.getSession(credentials)
+        .then(session => session.sql(queryString).execute())
+        .then(result => queryResultToJson(result))
+        .then(result => {
+            if (result.length == 0) return res.status(404).send(`User with id ${user_id} is not found`)
+            return res.json(result)
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).send('Server Error')
+        });
 });
 
 module.exports = router
