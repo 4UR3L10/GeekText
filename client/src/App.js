10e48cc98beaf8153e71e5a6112a54bdd11f9131
4UR3L10 <aureliomartinezdros@outlook.com>
2021-03-22 18:33:10 -0400

    InsertNavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "react-bootstrap/Navbar"; // NavBar ReactBootsrap.
+import Nav from "react-bootstrap/Nav"; // Nav ReactBootsrap.
+import NavDropdown from "react-bootstrap/NavDropdown"; // NavDropDown ReactBootsrap.
 
 function App() {
-  return (
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      {/* NavBar.*/}
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">GeekText</Navbar.Brand>
+        <Nav className="mr-auto">
+          <NavDropdown title="Book Browsing" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">By Genre</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Book Rating</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Top Sellers</NavDropdown.Item>
+         </NavDropdown>
+         <NavDropdown title="Book Browsing By Genre" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#">Literature</NavDropdown.Item>
+            <NavDropdown.Item href="#">Manga</NavDropdown.Item>
+           <NavDropdown.Item href="#">Romance</NavDropdown.Item>
+           <NavDropdown.Item href="#">Fiction</NavDropdown.Item>
+           <NavDropdown.Item href="#">Science</NavDropdown.Item>
+           <NavDropdown.Item href="#">Sci-Fi</NavDropdown.Item>
+         </NavDropdown>
+        </Nav>
+        <Nav>
+            <NavDropdown title="Profile" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Sign Up</NavDropdown.Item>
+              <NavDropdown.Item href="#">Sign In</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#">Settings</NavDropdown.Item>
+              <NavDropdown.Item href="#">Shipping Address</NavDropdown.Item>
+              <NavDropdown.Item href="#">Payment</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">SignOut</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">Shopping Cart</Nav.Link>
+            <Nav.Link href="#">WishList</Nav.Link>
+        </Nav>    
+      </Navbar>  
+      <br />
+    
+      {/* Other Stuff.*/}
+    
     </div>
   );
 }
