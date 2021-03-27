68e653dab7c77607963fd68d54dda8b16eb951ec
getrod <kevinlouisjean@gmail.com>
2021-03-27 18:49:46 -0400

    added author design to be similar to b&n

--- client/src/book-details/AuthorPage.jsx
+++ client/src/book-details/AuthorPage.jsx
@@ -5,6 +5,7 @@ import LoadingPage from './components/LoadingPage';
 import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import ListGroup from "react-bootstrap/esm/ListGroup";
 
 
 
@@ -15,10 +16,10 @@ function AuthorPage(props) {
 
     useEffect(() => {
         getAuthorInfo()
-        .then(() => {
-            getAuthorBooks();
-        })
-        
+            .then(() => {
+                getAuthorBooks();
+            })
+
     }, []);
 
     function getAuthorBooks() {
@@ -41,54 +42,84 @@ function AuthorPage(props) {
 
     function BookListItem(props) {
         return (
-            <a href={`http://localhost:3000/books/${books[props.idx].book_id}`} key={props.idx}>
-                <Media >
-                    <img
-                        width={64}
-                        height={64}
-                        className="mr-3"
-                        src={books[props.idx].cover}
-                        alt="Generic placeholder"
-                    />
-                    <Media.Body>
-                        <h5>{books[props.idx].book_title}</h5>
-                    </Media.Body>
-                </Media>
-            </a>
+            <ListGroup.Item key={props.idx}>
+                <Row>
+                    <Col md="auto">
+                        <a href={`http://localhost:3000/books/${books[props.idx].book_id}`}>
+                        <img
+                            src={books[props.idx].cover}
+                            alt="Generic placeholder"
+                            style={{
+                                maxHeight: "200px", maxWidth: "100%", padding: "5px",
+                            }}
+                        />
+                        </a>
+                    </Col>
+                    <Col>
+                        <div>
+                            <h5 className="gt-bd-title" style={{ fontSize: "1.3rem" }}>
+                                <a
+                                    class="text-dark"
+                                    href={`http://localhost:3000/books/${books[props.idx].book_id}`}
+                                >
+                                    {books[props.idx].book_title}
+                                    <span
+                                        style={{
+                                            color: "#727272",
+                                            fontWeight: "300",
+                                            fontSize: ".9375rem"
+                                        }}
+                                    >
+                                        {` (${books[props.idx].published_date.substring(0, 10)})`}
+                                    </span>
+                                </a>
+                                <h6>by <a href={`/author/${authorId}`} class="text-info">
+                                    {books[props.idx].author_name}
+                                </a> </h6>
+                                <h6><u>Rating:</u> {books[props.idx].avg_rating}</h6>
+
+                            </h5>
+                        </div>
+
+                    </Col>
+                </Row>
+            </ListGroup.Item>
 
         );
     }
 
     function AuthorDescription(props) {
         return (
-        <div style={{marginTop: "30px"}}{...props}>
-            <h1 className="gt-bd-title" style={{marginBottom: "10px"}}>
-                {`${author.author_name} Books`}
-            </h1>
-            <div dangerouslySetInnerHTML={{__html: author.author_bio}} 
-            style={{marginBottom: "30px"}}
-            />
-        </div>
+            <div style={{ marginTop: "30px" }}{...props}>
+                <h1 className="gt-bd-title" style={{ marginBottom: "10px" }}>
+                    {`${author.author_name} Books`}
+                </h1>
+                <div dangerouslySetInnerHTML={{ __html: author.author_bio }}
+                    style={{ marginBottom: "30px" }}
+                />
+            </div>
         )
     }
 
-    
+
     return (
         <Container style={{ maxWidth: "1024px", margin: "auto" }} >
-            <div ></div>
-            {author && <AuthorDescription /> }
+            {author && <AuthorDescription />}
             { books.length <= 0 ? <LoadingPage /> :
                 <div>
-                    <h2 className="gt-bd-title" style={{fontSize: "1.5rem", textAlign: "center"}}> 
+                    <h2 className="gt-bd-title" style={{ fontSize: "1.5rem", textAlign: "center", marginBottom: "20px" }}>
                         <Row>
                             <Col><hr></hr></Col>
                             <Col md="auto">All Books</Col>
                             <Col><hr></hr></Col>
                         </Row>
                     </h2>
-                    {books.map((book, idx) => (
-                        <BookListItem idx={idx} />
-                    ))}
+                    <ListGroup variant="flush">
+                        {books.map((book, idx) => (
+                            <BookListItem idx={idx} />
+                        ))}
+                    </ListGroup>
+
                 </div>
             }
         </Container>
