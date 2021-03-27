0ade42c866d5db882605e31f89efc4097c8f4afa
getrod <kevinlouisjean@gmail.com>
2021-03-27 17:44:20 -0400

    added author description to author page

--- client/src/book-details/AuthorPage.jsx
+++ client/src/book-details/AuthorPage.jsx
@@ -3,19 +3,43 @@ import Container from "react-bootstrap/esm/Container";
 import Media from 'react-bootstrap/Media';
 import LoadingPage from './components/LoadingPage';
 import { useParams } from "react-router-dom";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 
 
 
 function AuthorPage(props) {
     const { authorId } = useParams();
     const [books, setBooks] = useState([]);
+    const [author, setAuthor] = useState(null);
 
     useEffect(() => {
-        getAuthorBooks();
+        getAuthorInfo()
+        .then(() => {
+            getAuthorBooks();
+        })
+        
     }, []);
 
+    function getAuthorBooks() {
+        return fetch(`http://localhost:4000/api/authors/${authorId}/books`)
+            .then((response) => response.json())
+            .then((data) => {
+                setBooks(data)
+            })
+            .catch((err) => console.log(err));
+    };
+
+    function getAuthorInfo() {
+        return fetch(`http://localhost:4000/api/authors/${authorId}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setAuthor(data[0])
+            })
+            .catch((err) => console.log(err));
+    }
+
     function BookListItem(props) {
-        console.log(props);
         return (
             <a href={`http://localhost:3000/books/${books[props.idx].book_id}`} key={props.idx}>
                 <Media >
@@ -35,18 +59,33 @@ function AuthorPage(props) {
         );
     }
 
-    function getAuthorBooks() {
-        fetch(`http://localhost:4000/api/authors/${authorId}/books`)
-            .then((response) => response.json())
-            .then((data) => {
-                setBooks(data)
-            })
-            .catch((err) => console.log(err));
-    };
+    function AuthorDescription(props) {
+        return (
+        <div style={{marginTop: "30px"}}{...props}>
+            <h1 className="gt-bd-title" style={{marginBottom: "10px"}}>
+                {`${author.author_name} Books`}
+            </h1>
+            <div dangerouslySetInnerHTML={{__html: author.author_bio}} 
+            style={{marginBottom: "30px"}}
+            />
+        </div>
+        )
+    }
+
+    
     return (
         <Container style={{ maxWidth: "1024px", margin: "auto" }} >
+            <div ></div>
+            {author && <AuthorDescription /> }
             { books.length <= 0 ? <LoadingPage /> :
                 <div>
+                    <h2 className="gt-bd-title" style={{fontSize: "1.5rem", textAlign: "center"}}> 
+                        <Row>
+                            <Col><hr></hr></Col>
+                            <Col md="auto">All Books</Col>
+                            <Col><hr></hr></Col>
+                        </Row>
+                    </h2>
                     {books.map((book, idx) => (
                         <BookListItem idx={idx} />
                     ))}
